64150aaa02fbc3989d460e792f08e6f5f10d931b
Thien Pham <59156238+thienphamIT1907@users.noreply.github.com>
2023-09-16 01:13:35 +0700

    Test

--- api/serverless/handler.js
+++ api/serverless/handler.js
@@ -2,7 +2,7 @@ import { readFileSync } from "fs";
 import path from "path";
 
 export default function handler(request, response) {
-  const file = path.join(process.cwd(), "files", "/tmp/db.json");
+  const file = path.join(process.cwd(), "files", "tmp/db.json");
   const stringified = readFileSync(file, "utf8");
 
   response.setHeader("Content-Type", "application/json");
